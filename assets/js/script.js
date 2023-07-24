d2571781601cd781f609277502cf9dd54a49c870
Kaelyn P.J. Rasmussen <r4smussen.pj@gmail.com>
2023-07-24 16:52:56 -0600

    Added comments in js & html, styled bulma a bit more in html

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,37 @@
-var openWeatherKey = "2d52ba18533dcbc34d4b78e05f50aa2c";
+//var openWeatherKey = "2d52ba18533dcbc34d4b78e05f50aa2c";
 
+//Function to create elements based on API info
 function showWeather(weather) {
 var resultsCurrent = document.querySelector("#current-day");
-//Creating elements to show the current weather
 //Current date
 var currentDay = dayjs();
 $('#current-day').text(currentDay.format('dddd MM/DD/YYYY'));
-//Name of city via search
-var cityName = document.createElement("h2");
+//Creating elements for the current day
+var cityName = document.createElement("div");
+var humidity = document.createElement("div");
+var windSpeed = document.createElement("div");
+var temperature = document.createElement("div");
+//Setting attributes for styling to the new elements
+cityName.setAttribute("class", "title");
+temperature.setAttribute("class", "subtitle is-dark");
+windSpeed.setAttribute("class", "subtitle is-dark");
+humidity.setAttribute("class", "subtitle is-dark");
+//Setting the text content based off of information given via API
 cityName.textContent = weather.name;
-resultsCurrent.append(cityName);
-//Current temperature of said city
-var temperature = document.createElement("p");
 temperature.textContent = "Temp: " + weather.main.temp + " °F";
-resultsCurrent.append(temperature);
-//Current wind speed of said city
-var windSpeed = document.createElement("p");
 windSpeed.textContent = "Wind: " + weather.wind.speed + " MPH";
-resultsCurrent.append(windSpeed);
-//Current humidity of said city
-var humidity = document.createElement("p");
 humidity.textContent = "Humidity: " + weather.main.humidity + "%";
+//Appending elements to the GUI
+resultsCurrent.append(cityName);
+resultsCurrent.append(temperature);
+resultsCurrent.append(windSpeed);
 resultsCurrent.append(humidity);
 };
 
 function getWeather(query) {
 var test = "https://api.openweathermap.org/data/2.5/weather?q=London&units=imperial&appid=2d52ba18533dcbc34d4b78e05f50aa2c";
 //var test = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&units=imperial&appid=2d52ba18533dcbc34d4b78e05f50aa2c";
+
 //add a variable for query so user can input a city name
 //add eventlistener to search button to run the show weather and get weather funcs
 fetch(test)
@@ -35,3 +40,23 @@ fetch(test)
 };
 
 getWeather();
+
+//Selecting the Search button
+//var submitSearch = document.querySelector("#submit");
+
+//Getting items to store based on user input
+//var userSearch = JSON.parse(localStorage.getItem("userSearch")) || [];
+
+//userSearch = document.querySelector("#search");
+
+//submitSearch.addEventListener("click", getWeather);
+
+// submitSearch.addEventListener("click", function() {
+//     userSearch.push(initialsEl.value + ": " + score);
+// localStorage.setItem("userSearch", JSON.stringify(userSearch));
+//     for (let i = 0; i < userSearch.length; i++) {
+//         var li = document.createElement("li")
+//         li.textContent = userSearch[i];
+//         li.setAttribute("id", "recent");
+//         document.querySelector("#recent").appendChild(li);
+//         };
